fa0c7ee516bc91abfc6d2fb6625870a8d3c0bf4d
Nadeem Mahmood <mahmoodn@purdue.edu>
2016-10-06 20:41:20 -0400

    maps working

--- Citi/MapViewController.swift
+++ Citi/MapViewController.swift
@@ -11,10 +11,13 @@ import GoogleMaps
 import Foundation
 import CoreLocation
 import MapKit
+import AWSDynamoDB
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
 
-    @IBOutlet weak var viewMap: GMSMapView!
+    @IBOutlet weak var mapView: GMSMapView!
+    var userMarker: GMSMarker?
+    var userView: UIImageView?
     
     var locationManager = CLLocationManager()
     var didFindMyLocation = false
@@ -43,19 +46,51 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         let userLocation = locations.last
         let center = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
         
-        let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
-                                                          longitude: userLocation!.coordinate.longitude, zoom: 8)
-        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        mapView.camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
+                                                          longitude: userLocation!.coordinate.longitude, zoom: 15)
         mapView.isMyLocationEnabled = true
-        self.view = mapView
+        mapView.settings.compassButton = true
+        mapView.settings.myLocationButton = true
         
-        let marker = GMSMarker()
-        marker.position = center
-        marker.title = "Current Location"
-        marker.snippet = "locations = \(center.latitude) \(center.longitude)"
+        let userIcon = UIImage(named: "username_icon")!.withRenderingMode(.alwaysTemplate)
+        let markerView = UIImageView(image: userIcon)
+        markerView.tintColor = UIColor.red
+        self.userView = markerView
+        
+        
+        let marker = GMSMarker(position: center)
+        marker.title = "Your Location"
+        marker.snippet = "\(center.latitude) \(center.longitude)"
+        //marker.iconView = markerView
+        marker.isFlat = true
+        marker.icon = UIImage(named: "username_icon")
+        marker.tracksViewChanges = true
         marker.map = mapView
+        self.userMarker = marker
+        
+        findCloseDrivers();
         
         locationManager.stopUpdatingLocation()
+    }
+    
+    func mapView(mapView: GMSMapView, idleAtCameraPosition position: GMSCameraPosition) {
+        UIView.animate(withDuration: 5.0, animations: { () -> Void in
+            self.userView?.tintColor = UIColor.blue
+            }, completion: {(finished: Bool) -> Void in
+                // Stop tracking view changes to allow CPU to idle.
+                self.userMarker?.tracksViewChanges = false
+        })
+    }
+    
+    func findCloseDrivers() {
+        let position = CLLocationCoordinate2D(latitude: 40.423527, longitude: -86.906319)
+        let marker = GMSMarker(position: position)
+        marker.icon = UIImage(named: "driving")
+        marker.isFlat = true
+        //marker.icon = UIImage(named: "username_icon")
+        marker.tracksViewChanges = true
+        marker.map = mapView
+
     }
     
     
